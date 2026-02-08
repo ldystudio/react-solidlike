ae80ba4fccaeffef806ac24a0a3a8f6d4f03d688
Liudy <1187551003@qq.com>
2026-02-08 17:26:02 +0800

    fix: 补充缺失的 ClientOnly 和 Once 组件导出
      主要变更：
      - 添加 ClientOnly 组件到 index.ts 导出
      - 添加 Once 组件到 index.ts 导出
      - 升级 rolldown 至 1.0.0-rc.3
      - 版本号升级至 2.5.3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export { Await, type AwaitProps } from "./Await";
+export { ClientOnly, type ClientOnlyProps } from "./ClientOnly";
 export { Dynamic, type DynamicProps } from "./Dynamic";
 export { ErrorBoundary, type ErrorBoundaryProps } from "./ErrorBoundary";
 export { For, type ForProps } from "./For";
+export { Once, type OnceProps } from "./Once";
 export { QueryBoundary, type QueryBoundaryProps, type QueryResult } from "./QueryBoundary";
 export { Repeat, type RepeatProps } from "./Repeat";
 export { Show, type ShowProps } from "./Show";
